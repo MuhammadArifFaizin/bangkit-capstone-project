68fbaf4b17bb3d012b883d6564747cfde98db822
ZalsabilaBudi <zalsabila-2018@fst.unair.ac.id>
2022-06-08 19:33:23 +0700

    md: finish splash screen

--- mential-app/app/src/main/java/com/example/mential/SplashScreen.kt
+++ mential-app/app/src/main/java/com/example/mential/SplashScreen.kt
@@ -1,11 +1,37 @@
 package com.example.mential
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mential.databinding.ActivitySplashScreenBinding
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.logoApp.alpha = 0f
+        binding.logoApp.animate().setDuration(5000).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+        binding.titleApp.alpha = 0f
+        binding.titleApp.animate().setDuration(5000).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+        binding.mottoApp.alpha = 0f
+        binding.mottoApp.animate().setDuration(5000).alpha(1f).withEndAction {
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
     }
 }
