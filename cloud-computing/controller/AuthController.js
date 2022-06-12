27cce46342665505e9b4c33507ae74b7c8a4b817
auliafaturrachman <afatur.code@gmail.com>
2022-06-12 12:13:00 +0700

    cc: change request to async

--- cloud-computing/controller/AuthController.js
+++ cloud-computing/controller/AuthController.js
@@ -8,9 +8,8 @@ const conPrivate = path.join(__dirname, '../config/private.pem');
 const pool = new Pool(db);
 
 const login = async (req, res) => {
-  const { email, password } = req.body;
-
-  try{
+  try {
+    const { email, password } = req.body;
     const user = await pool.query(
       'SELECT * FROM users WHERE email = $1 AND password = $2',
       [email, password]
@@ -38,41 +37,46 @@ const login = async (req, res) => {
   }
 };
 
-const registration = (req, res) => {
-  const { name, email, password, gender, date_birth, city } = req.body;
-    
-  pool.query(
-    'INSERT INTO users (name, email, password, gender, date_birth, city) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
-    [name, email, password, gender, date_birth, city],
-    (err, result) => {
-      if (err) {
-        res.status(400)
-          .contentType('application/json')
-          .json({ status: err.name, message: err.message });
-      } else {
-        res.status(201)
-          .contentType('application/json')
-          .json({
-            status: 'success',
-            // message: 'registration successful',
-            data: result.rows[0].id,
-          });
-      }
-    },
-  );
+const registration = async (req, res) => {
+  try {
+    const { name, email, password, gender, date_birth, city } = req.body;
+    const result = await pool.query(
+      'INSERT INTO users (name, email, password, gender, date_birth, city) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
+      [name, email, password, gender, date_birth, city]);
+
+    res.status(201)
+    .contentType('application/json')
+    .json({
+      status: 'success',
+      message: 'registration successful',
+      // data: result.rows[0].id,
+    });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({ 
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
-const getRegisterUserById = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (err, results) => {
-    if (err) {
-      res.status(400)
-        .contentType('application/json')
-        .json({ status: err.name, message: err.message });
-    } else {
-      res.status(200).json({ status: 'success', data: results.rows });
-    }
-  });
+const getRegisterUserById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const results = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    res.status(200).json({ 
+      status: 'success',
+      data: results.rows
+    });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({ 
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
 module.exports = {
--- cloud-computing/controller/UserController.js
+++ cloud-computing/controller/UserController.js
@@ -3,99 +3,111 @@ const db = require("../config/database");
 
 const pool = new Pool(db);
 
-const getUser = (req, res) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (err, results) => {
-    if (err) {
-      res.status(400)
-        .contentType('application/json')
-        .json({ status: err.name, message: err.message });
-    } else {
-      res.status(200)
-        .contentType('application/json')
-        .json({ status: 'success', data: results.rows });
-    }
-  });
+const getUser = async (req, res) => {
+  try {
+    const results = await pool.query('SELECT * FROM users ORDER BY id ASC');
+
+    res.status(200)
+      .contentType('application/json')
+      .json({ 
+        status: 'success',
+        data: results.rows
+      });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({ 
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
-const getUserById = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  pool.query('SELECT * FROM users WHERE id = $1', [id], (err, results) => {
-    if (err) {
-      res.status(400)
-        .contentType('application/json')
-        .json({ status: err.name, message: err.message });
-    } else {
-      res.status(200)
-        .contentType('application/json')
-        .json({ status: 'success', data: results.rows });
-    }
-  });
+const getUserById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const results = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+
+    res.status(200)
+      .contentType('application/json')
+      .json({ status: 'success', data: results.rows });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
-const createUser = (req, res) => {
-  const { name, email } = req.body;
-    
-  pool.query(
-    'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id',
-    [name, email],
-    (err, result) => {
-      if (err) {
-        res.status(400)
-          .contentType('application/json')
-          .json({ status: err.name, message: err.message });
-      } else {
-        res.status(201)
-          .contentType('application/json')
-          .json({
-            status: 'success',
-            data: result.rows[0].id,
-          });
-      }
-    },
-  );
+const createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    const result = await pool.query(
+      'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING id',
+      [name, email]);
+
+    res.status(201)
+      .contentType('application/json')
+      .json({
+        status: 'success',
+        data: result.rows[0].id,
+      });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
-const updateUser = (req, res) => {
-  const id = parseInt(req.params.id, 10);
-  const { name, email } = req.body;
+const updateUser = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const { name, email } = req.body;
+  
+    const result = await pool.query(
+      'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+      [name, email, id]);
 
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (err) => {
-      if (err) {
-        res.status(400)
-          .contentType('application/json')
-          .json({ status: err.name, message: err.message });
-      } else {
-        res.status(200)
-          .contentType('application/json')
-          .json({
-            status: 'success',
-            message: `User modified with ID: ${id}`,
-          });
-      }
-    },
-  );
+    res.status(200)
+      .contentType('application/json')
+      .json({
+        status: 'success',
+        message: `User modified with ID: ${id}`,
+      });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
-const deleteUser = (req, res) => {
-  const id = parseInt(req.params.id, 10);
+const deleteUser = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    const result = await pool.query('DELETE FROM users WHERE id = $1', [id]);
 
-  pool.query('DELETE FROM users WHERE id = $1', [id], (err) => {
-    if (err) {
-      res.status(400)
-        .contentType('application/json')
-        .json({ status: err.name, message: err.message });
-    } else {
-      res.status(200)
-        .contentType('application/json')
-        .json({
-          status: 'success',
-          message: `User deleted with ID: ${id}`,
-        });
-    }
-  });
+    res.status(200)
+      .contentType('application/json')
+      .json({
+        status: 'success',
+        message: `User deleted with ID: ${id}`,
+      });
+  } catch (err) {
+    res.status(400)
+      .contentType('application/json')
+      .json({
+        status: err.name,
+        message: err.message
+      });
+  }
 };
 
 module.exports = {
