992a0e4a50efafdfc328e345110f25fcd566107c
ameliaame <amelia191299@gmail.com>
2022-06-12 16:17:55 +0700

    md: cautioname

--- mential-app/app/src/main/java/com/example/mential/CautionActivity.kt
+++ mential-app/app/src/main/java/com/example/mential/CautionActivity.kt
@@ -4,17 +4,18 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mential.databinding.ActivityCautionBinding
-import com.example.mential.databinding.ActivityLandingPage1Binding
 
 class CautionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCautionBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding= ActivityCautionBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_caution)
 
-//        binding.btnSkip1.setOnClickListener{
-//            startActivity(Intent(this@CautionActivity, Test1Activity::class.java))
-//        }
+        binding.btnStarttest.setOnClickListener{
+            startActivity(Intent(this@CautionActivity, Test1Activity::class.java))
+        }
     }
 }
